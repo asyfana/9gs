b9f635fa1c60817ce3a369b80047c2f408a3ce21
asyfana <108052294+asyfana@users.noreply.github.com>
2024-02-06 21:28:08 +0800

    Add files via upload

---
+++ cpm.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import warnings
+import sys
+# import plotly.figure_factory as ff
+
+
+warnings.filterwarnings("ignore")
+
+st.title("OS data and CPM")
+
+
+st.image("C:/Users/Asus/Pictures/cpm.png", caption='CPM')
