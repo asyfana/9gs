7565358981188414f78442b213945778e1780be5
asyfana <108052294+asyfana@users.noreply.github.com>
2024-02-06 22:33:39 +0800

    Update cpm.py

--- cpm.py
+++ cpm.py
@@ -23,23 +23,18 @@ if (choose == "CPM"):
 elif (choose == "OS data"):
     st.title("OS data")
 
-    form = st.form("side_form")
-    data_type = form.radio("Select data type", ["Capacitance", "Resistance", "Inductance"])
-    form.form_submit_button("Submit")
-
-    if data_type == "Capacitance":
-        st.header("Capacitance data")
-        df = pd.read_csv("Capacitance_Data.csv")
-        st.dataframe(df)
+    st.header("Capacitance data")
+    df = pd.read_csv("Capacitance_Data.csv")
+    st.dataframe(df)
    
-        #permittivity = pd.read_csv("Permittivity.csv")
-        df2 = pd.read_csv("Capacitance1.csv")
-        #st.dataframe(df2)
+    #permittivity = pd.read_csv("Permittivity.csv")
+    df2 = pd.read_csv("Capacitance1.csv")
+    #st.dataframe(df2)
 
-        freq_slider_start = st.slider("Slider for Start Frequency", 50, 100000, 50)
-        freq_slider_end = st.slider("Slider for End Frequency", freq_slider_start, 100000, 100000)
-        filtered_capacitance = df2.loc[(df2['Frequency'] >= freq_slider_start) & (df2['Frequency'] <= freq_slider_end)]
+    freq_slider_start = st.slider("Slider for Start Frequency", 50, 100000, 50)
+    freq_slider_end = st.slider("Slider for End Frequency", freq_slider_start, 100000, 100000)
+    filtered_capacitance = df2.loc[(df2['Frequency'] >= freq_slider_start) & (df2['Frequency'] <= freq_slider_end)]
             
-        st.header("Capacitance(Cp)")
-        st.line_chart(filtered_capacitance, x="Frequency", use_container_width=True)
+    st.header("Capacitance(Cp)")
+    st.line_chart(filtered_capacitance, x="Frequency", use_container_width=True)
     
