1bc5d2585bcfeefcf433b3f25847751f3aab3bd7
asyfana <108052294+asyfana@users.noreply.github.com>
2024-02-07 09:53:34 +0800

    Update cpm.py

--- cpm.py
+++ cpm.py
@@ -26,53 +26,67 @@ if (choose == "CPM"):
 elif (choose == "OS data"):
     st.title("OS data")
 
-    st.header("Capacitance data")
-    df = pd.read_csv("Capacitance Data.csv")
-    st.dataframe(df)
+    # st.header("Capacitance data")
+    # df = pd.read_csv("Capacitance Data.csv")
+    # st.dataframe(df)
    
     #permittivity = pd.read_csv("Permittivity.csv")
     df2 = pd.read_csv("Capacitance1.csv")
     #st.dataframe(df2)
 
-    freq_slider_start = st.slider("Slider for Start Frequency", 50, 100000, 50)
-    freq_slider_end = st.slider("Slider for End Frequency", freq_slider_start, 100000, 100000)
-    filtered_capacitance = df2.loc[(df2['Frequency'] >= freq_slider_start) & (df2['Frequency'] <= freq_slider_end)]
+    # freq_slider_start = st.slider("Slider for Start Frequency", 50, 100000, 50)
+    # freq_slider_end = st.slider("Slider for End Frequency", freq_slider_start, 100000, 100000)
+    # filtered_capacitance = df2.loc[(df2['Frequency'] >= freq_slider_start) & (df2['Frequency'] <= freq_slider_end)]
             
     #st.header("Capacitance(Cp)")
-    st.line_chart(filtered_capacitance, x="Frequency", use_container_width=True)
+    # st.line_chart(filtered_capacitance, x="Frequency", use_container_width=True)
 
 
-    st.header("Inductance data")
-    df3 = pd.read_csv("Inductance Data.csv")
-    st.dataframe(df3)
+    # st.header("Inductance data")
+    # df3 = pd.read_csv("Inductance Data.csv")
+    # st.dataframe(df3)
    
     #permittivity = pd.read_csv("Permittivity.csv")
     df4 = pd.read_csv("Inductance1.csv")
     #st.dataframe(df2)
 
-    filtered_Inductance = df4.loc[(df4['Frequency'] >= freq_slider_start) & (df4['Frequency'] <= freq_slider_end)]
-    st.line_chart(filtered_Inductance, x="Frequency", use_container_width=True)
+    # filtered_Inductance = df4.loc[(df4['Frequency'] >= freq_slider_start) & (df4['Frequency'] <= freq_slider_end)]
+    # st.line_chart(filtered_Inductance, x="Frequency", use_container_width=True)
 
 
 
-    st.header("Resistance data")
-    df5 = pd.read_csv("Resistance Data.csv")
-    st.dataframe(df5)
+    # st.header("Resistance data")
+    # df5 = pd.read_csv("Resistance Data.csv")
+    # st.dataframe(df5)
    
 
     df6 = pd.read_csv("Resistance1.csv")
 
 
-    filtered_Resistance = df6.loc[(df6['Frequency'] >= freq_slider_start) & (df6['Frequency'] <= freq_slider_end)]
-    st.line_chart(filtered_Resistance, x="Frequency", use_container_width=True)
+    # filtered_Resistance = df6.loc[(df6['Frequency'] >= freq_slider_start) & (df6['Frequency'] <= freq_slider_end)]
+    # st.line_chart(filtered_Resistance, x="Frequency", use_container_width=True)
 
 #..........................................................................................................................................................
 
     filtered_capacitance = df2.loc[(df2['Frequency'] >= freq_slider_start) & (df2['Frequency'] <= freq_slider_end)]
     label = ":blue[Chart for capacitance] " 
 
+    filtered_Inductance = df4.loc[(df4['Frequency'] >= freq_slider_start) & (df4['Frequency'] <= freq_slider_end)]
+    label1 = ":blue[Chart for Inductance] " 
+
+    label1 = ":blue[Chart for Resistance] " 
+    filtered_Resistance = df6.loc[(df6['Frequency'] >= freq_slider_start) & (df6['Frequency'] <= freq_slider_end)]
+
     
     with st.expander(label):
         st.line_chart(filtered_capacitance, x="Frequency", use_container_width=True)
-        st.write("capacitance vs frequency")
+        st.write("capacitance vs frequency whole data")
         st.dataframe(df)
+     with st.expander(label1):
+        st.line_chart(filtered_Inductance, x="Frequency", use_container_width=True)
+        st.write("Inductance vs frequency whole data")
+        st.dataframe(df3)
+    with st.expander(label1):
+        st.line_chart(filtered_Resistance, x="Frequency", use_container_width=True)
+        st.write("Resistance vs frequency whole data")
+        st.dataframe(df6)
